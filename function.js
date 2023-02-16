0af9b134eeb2da402b954c7502c787b7dd186a51
Andressa <dessa_trindade12@hotmail.com>
2023-02-16 12:01:51 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,41 @@
+const user = {
+  username: "",
+  password: ""
+}
+const usuarioCorreto = {
+  nameadd: "Laura",
+  senha: "1234"
+};
+
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+
+
+const handleChangeUserName = (e) => {user.username=e.target.value}
+const handleChangePassword = (e) => {user.password=e.target.value}
+
+
+username.addEventListener('change', handleChangeUserName )
+password.addEventListener('change', handleChangePassword)
+
+const form = document.querySelector('#form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  clicou();
+})
+
+function clicou () {
+  const db = JSON.parse(localStorage.getItem('db'))
+
+      if (user.username == db.nameadd && user.password == db.senha) {
+          location.href = './button.html';
+ 
+      }else {
+          alert("Credenciais incorretas")
+  }
+};
+
+window.onload = () => {
+  localStorage.setItem('db',JSON.stringify(usuarioCorreto))
+}
